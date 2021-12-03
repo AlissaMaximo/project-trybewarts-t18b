67c4317860fefbcc7ce62095d1e0b98d428611f2
Ingrid Mattos <ingrid_matt@hotmail.com>
2021-12-02 22:57:28 -0300

    requisitos finais

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const email = document.querySelector('.email');
 const senha = document.querySelector('.senha');
-const button = document.querySelector('.button');
-
 function login() {
-//   event.preventDefault();
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
   }
 }
+const button = document.querySelector('.button');
 button.addEventListener('click', login);
+// O type checkbox vem com um atributo de checked que retorna true ou falso.
+
+function checkedBox() {
+  const btn = document.querySelector('#submit-btn');
+  const check = document.querySelector('#agreement');
+  if (check.checked === true) {
+    btn.disabled = false;
+  } else {
+    btn.disabled = true;
+  }
+}
+const check = document.querySelector('#agreement');
+check.addEventListener('click', checkedBox);
