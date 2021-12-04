5921e4a245033c7fdb129d688bdc8493f2f02b9f
AlissaMaximo <alissahiratamaximo@gmail.com>
2021-12-03 23:41:23 -0300

    Alterei algumas variáveis e uma função para organização.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const email = document.querySelector('.email');
 const senha = document.querySelector('.senha');
+const buttonLogin = document.querySelector('.button');
+const check = document.querySelector('#agreement');
+const buttonForm = document.querySelector('#submit-btn');
+
 function login() {
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
     alert('Olá, Tryber!');
@@ -7,18 +11,18 @@ function login() {
     alert('Email ou senha inválidos.');
   }
 }
-const button = document.querySelector('.button');
-button.addEventListener('click', login);
+
+buttonLogin.addEventListener('click', login);
+
 // O type checkbox vem com um atributo de checked que retorna true ou falso.
 
-function checkedBox() {
-  const btn = document.querySelector('#submit-btn');
+function checkBox() {
   const check = document.querySelector('#agreement');
   if (check.checked === true) {
-    btn.disabled = false;
+    buttonForm.disabled = false;
   } else {
-    btn.disabled = true;
+    buttonForm.disabled = true;
   }
 }
-const check = document.querySelector('#agreement');
-check.addEventListener('click', checkedBox);
+
+check.addEventListener('click', checkBox);
